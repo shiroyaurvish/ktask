03e0601eef411dd1d5bd8eaac4756ac02ce96704
Urvish <urvishshiroya262000@gmail.com>
2023-10-12 16:13:15 +0530

    app added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Web3 from "web3";
+import "./App.css";
+import TransectionComponent from "./TransectionComponent";
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [ethBalance, setEthBalance] = useState("");
+
+  const detectCurrentProvider = () => {
+    let provider;
+    if (window.ethereum) {
+      provider = window.ethereum;
+    } else if (window.web3) {
+      provider = window.web3.currentProvider;
+    } else {
+      console.log("Non-ethereum browser detected. You should install Metamask");
+    }
+    return provider;
+  };
+
+  const onConnect = async () => {
+    try {
+      const currentProvider = detectCurrentProvider();
+      if (currentProvider) {
+        await currentProvider.request({ method: "eth_requestAccounts" });
+        const web3 = new Web3(currentProvider);
+        const userAccount = await web3.eth.getAccounts();
+        const account = userAccount[0];
+        let ethBalance = await web3.eth.getBalance(account);
+        setEthBalance(ethBalance);
+        setIsConnected(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const onDisconnect = () => {
+    setIsConnected(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* Task 1 */}
+      <div>
+        <div className="app-header">
+          <h1>Task Application</h1>
+          <h2>Connect With Metamask</h2>
+        </div>
+        <div className="app-wrapper">
+          {!isConnected && (
+            <div>
+              <button className="app-button__login" onClick={onConnect}>
+                Login
+              </button>
+            </div>
+          )}
+        </div>
+        {isConnected && (
+          <div className="app-wrapper">
+            <div className="app-details">
+              <h2> You are connected to metamask.</h2>
+              <div className="app-balance">
+                <span>Balance: </span>
+                {ethBalance}
+              </div>
+            </div>
+            <div>
+              <button className="app-buttons__logout" onClick={onDisconnect}>
+                Disconnect
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+      {/* Task 2 */}
+      <div>
+        <TransectionComponent />
+      </div>
     </div>
   );
 }
---
+++ src/TransectionComponent.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { ethers } from "ethers";
+import ErrorMessage from "./ErrorMessage";
+import TxList from "./TxList";
+
+const startPayment = async ({ setError, setTxs, ether, addr }) => {
+  try {
+    if (!window.ethereum)
+      throw new Error("No crypto wallet found. Please install it.");
+
+    await window.ethereum.send("eth_requestAccounts");
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    ethers.utils.getAddress(addr);
+    const tx = await signer.sendTransaction({
+      to: addr,
+      value: ethers.utils.parseEther(ether),
+    });
+    console.log({ ether, addr });
+    console.log("tx", tx);
+    setTxs([tx]);
+  } catch (err) {
+    setError(err.message);
+  }
+};
+
+export default function TransectionComponent() {
+  const [error, setError] = useState();
+  const [txs, setTxs] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setError();
+    await startPayment({
+      setError,
+      setTxs,
+      ether: data.get("ether"),
+      addr: data.get("addr"),
+    });
+  };
+
+  return (
+    <form className="" onSubmit={handleSubmit}>
+      <div className="">
+        <main className="">
+          <h1 className="">Send ETH payment</h1>
+          <div className="">
+            <div className="">
+              <input
+                type="text"
+                name="addr"
+                className=""
+                placeholder="Recipient Address"
+              />
+            </div>
+            <div className="my-3">
+              <input
+                name="ether"
+                type="text"
+                className=""
+                placeholder="Amount in ETH"
+              />
+            </div>
+          </div>
+        </main>
+        <footer className="p-4">
+          <button type="submit" className="transbtn">
+            Send now
+          </button>
+          <ErrorMessage message={error} />
+          <TxList txs={txs} />
+        </footer>
+      </div>
+    </form>
+  );
+}
